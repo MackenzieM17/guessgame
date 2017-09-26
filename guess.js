5c9e7c2d007e27b1117a15c43350cd6a40f31ed3
MackenzieM17 <32307291+MackenzieM17@users.noreply.github.com>
2017-09-26 14:10:44 -0400

    Guess with feedback

--- guess.js
+++ guess.js
@@ -1,4 +1,14 @@
 /* My new guessing game */
-alert ("Guessing game's start");
-Answer =  Math.round((Math.random()*100)+ 1);
+alert ("The guessing game's start");
+var Answer =  Math.round((Math.random()*100)+ 1);
 alert (Answer);
+var Guess = prompt("Guess a number between 1 and 100");
+if (Guess==Answer){
+	alert ("Your answer is correct");
+}
+if (Guess< Answer){
+alert ("Guess Higher");
+}
+if (Guess> Answer){
+alert ("Guess Lower");
+}
