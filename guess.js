9d8396c5fc517483b3544ca2fa2ba7460dbfc0e6
MackenzieM17 <32307291+MackenzieM17@users.noreply.github.com>
2017-09-27 08:19:06 -0400

    guessing correct

--- guess.js
+++ guess.js
@@ -1,14 +1,18 @@
 /* My new guessing game */
 alert ("The guessing game's start");
 var Answer =  Math.round((Math.random()*100)+ 1);
+var Turns = 0;
 alert (Answer);
-var Guess = prompt("Guess a number between 1 and 100");
-if (Guess==Answer){
-	alert ("Your answer is correct");
+do{
+	var Guess = prompt("Guess a number between 1 and 100");
+	Turns = Turns+1;
+	if (Guess < Answer){
+		alert ("Guess Higher");
+	}
+	else if (Guess > Answer){
+		alert ("Guess Lower");
+	}
 }
-if (Guess< Answer){
-alert ("Guess Higher");
-}
-if (Guess> Answer){
-alert ("Guess Lower");
-}
+while (Guess != Answer);
+alert ("Your answer is correct in "+Turns+" turns.");
+
