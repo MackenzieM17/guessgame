b86950b1bc6f3309df62f9331d447b35f27b64f7
MackenzieM17 <32307291+MackenzieM17@users.noreply.github.com>
2017-09-27 09:13:43 -0400

    Averages correct

--- guess.js
+++ guess.js
@@ -2,6 +2,9 @@
 alert ("The guessing game's start");
 var Answer =  Math.round((Math.random()*100)+ 1);
 var Turns = 0;
+var Games = 0;
+var TotalTurns = 0;
+var Average = TotalTurns / Games;
 alert (Answer);
 do{
 	var Guess = prompt("Guess a number between 1 and 100");
@@ -14,5 +17,9 @@ do{
 	}
 }
 while (Guess != Answer);
-alert ("Your answer is correct in "+Turns+" turns.");
-
+alert ("Your answer is correct in "+Turns+" Turn.");
+Games = Games + 1; 
+TotalTurns = TotalTurns + Turns;
+Average = TotalTurns / Games;
+alert ("It takes you  "+Turns+" Turns");
+alert ("The average is "+Average+" Average");
